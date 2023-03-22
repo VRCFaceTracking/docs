8acb8688450e53f6053a28fee5f8b3b67e318ae2
benaclejames <benaclejames@gmail.com>
2023-03-22 18:47:09 +0000

    Add algolia crawler

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,6 +85,34 @@ const config = {
               'aria-label': 'GitHub',
             }
           ],
+
+        },
+        algolia: {
+          // The application ID provided by Algolia
+          appId: 'IG7PS0550Z',
+
+          // Public API key: it is safe to commit it
+          apiKey: 'ff142e454687ae4be29e2a6080754a7f',
+
+          indexName: 'vrcft',
+
+          // Optional: see doc section below
+          contextualSearch: true,
+
+          // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+          externalUrlRegex: 'external\\.com|domain\\.com',
+
+          // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+          replaceSearchResultPathname: {
+            from: '/docs/', // or as RegExp: /\/docs\//
+            to: '/',
+          },
+
+          // Optional: Algolia search parameters
+          searchParameters: {},
+
+          // Optional: path for search page that enabled by default (`false` to disable it)
+          searchPagePath: 'search',
         },
         footer: {
           style: 'dark',
