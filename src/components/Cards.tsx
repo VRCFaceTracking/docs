5dc534e5887f048ceab840b410454cdf1f863763
kusomaigo <kusomaigo@gmail.com>
2023-04-29 01:00:59 -0700

    nicer 'install' link on vrcft page

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
 import Heading from '@theme/Heading';
+import clsx from 'clsx';
 
 
 interface Props {
     name: string;
     image: string;
     url: string;
-    urlTS: string;
     description: JSX.Element;
 }
   
@@ -21,5 +21,31 @@ export function PageCard({name, url, description}: Props) {
                 <p class="text--truncate cardDescription_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module">{description}</p>
             </Link>
         </div>
+    );
+}
+
+// More like the example playground card, but we'll use for general bit important links!
+export function ImageCard({name, image, url, description}: Props) {
+    return (
+      <div className="col col--6 margin-bottom--lg">
+        <div className={clsx('card')}>
+          <div className={clsx('card__image')}>
+            <Link to={url}>
+              <img src={image} alt={`${name}'s image`} />
+            </Link>
+          </div>
+          <div className="card__body">
+            <Heading as="h3">{name}</Heading>
+            <p>{description}</p>
+          </div>
+          <div className="card__footer">
+            <div className="button-group button-group--block">
+              <Link className="button button--secondary" to={url}>
+                Download
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 }
